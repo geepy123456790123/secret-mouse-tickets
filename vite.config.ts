6752b418883428c9c66e09f79719037348107dac
Grant Radermacher <grantradermacher@plex-mac-mini-8.local>
2026-07-03 00:59:39 -0500

    Configure Cloudflare production vars

--- vite.config.ts
+++ vite.config.ts
@@ -32,6 +32,18 @@ const localBindingConfig = {
         },
       ]
     : [],
+  vars: {
+    DAILY_PURCHASE_LIMIT: "25",
+    FROM_EMAIL: "Secret Mouse Tickets <hello@secretmousetickets.com>",
+    GOOGLE_SEARCH_URL:
+      "https://www.google.com/search?q=site:disneyevent.com&client=safari&sca_esv=2802dad21c2aa82f&sca_upv=1&hl=en-us&prmd=bsivn&sxsrf=ACQVn09cFK7nJ-G_9_qmduXCndQf3zYNyw:1709145310580&filter=0&biw=393&bih=741&dpr=3#ip=1",
+    SEARCH_NORMALIZER_PROVIDER: "serper",
+    SQUARE_APPLICATION_ID: "sq0idp-skrChnsqOFISfDV3KK4wVg",
+    SQUARE_ENVIRONMENT: "production",
+    SQUARE_LOCATION_ID: "LZGZRXXHY8TG0",
+    SQUARE_WEBHOOK_NOTIFICATION_URL:
+      "https://secretmousetickets.com/api/square/webhook",
+  },
 };
 
 export default defineConfig({
